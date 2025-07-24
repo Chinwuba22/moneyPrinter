e9778ca916b0d036608c8408ccaca51b8da7a6eb
Chris <christopherakwuba4@gmail.com>
2025-07-24 22:47:59 +0100

    Describe what you changed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  experimental: {
+    esmExternals: 'loose', // <- allows ESM modules to be imported
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
+
